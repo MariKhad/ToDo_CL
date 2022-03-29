f8c600272101eb580cf4d1b69106b3376ee336be
Marina Khadieva <utabond89@gmail.com>
2022-03-29 11:16:39 +0300

    Рабочий вариант

--- js/todo_cl.js
+++ js/todo_cl.js
@@ -1,78 +1,116 @@
+getTasks();
+
 import { TODO_UI } from './view.js';
 import { createTaskNode } from './view.js';
 
+const DONE = "done";
+const IN_PROGRESS = "in progress";
+
 let BUTTONS_DEL = document.querySelectorAll('.todo__btn--del');
 let CHECKBOXES = document.querySelectorAll('.box');
-let taskList = [];
 
-
-allTasksAdd();
+tasksAdd();
 checkedStyle();
 taskDelete();
+
+function getTasks() {
+	let tasks = Object.keys(localStorage);
+	let isChecked;
+	for (let task of tasks) {
+		task = JSON.parse(localStorage.getItem(task));
+		console.log(task.status);
+		if (task.status === 'done') {
+			isChecked = true;
+		} else {
+			isChecked = false;
+		}
+		let div = createTaskNode(task.name, isChecked);
+		if (task.priority === 'high') {
+			TODO_UI.HIGH.append(div);
+		} else {
+			TODO_UI.LOW.append(div);
+		}
+	}
+}
 
 
 
 
 function taskDelete() {
 	for (let button of BUTTONS_DEL) {
-		button.addEventListener('click', function (event) {
-			const targetTask = event.target;
-			let taskName = targetTask.previousSibling.textContent;
-			taskList = taskList.filter(item => item.name !== taskName);
-			console.log(taskList);
-			targetTask.parentElement.remove();
+		button.addEventListener('click', function () {
+			const currentTaskName = button.previousSibling.textContent;
+			localStorage.removeItem(currentTaskName);
+			button.parentElement.remove();
 			BUTTONS_DEL = document.querySelectorAll('.todo__btn--del');
 			CHECKBOXES = document.querySelectorAll('.box');
 		})
 	}
 
-	return taskList, CHECKBOXES, BUTTONS_DEL;
+	return CHECKBOXES, BUTTONS_DEL;
 }
 
 function checkedStyle() {
 	for (let box of CHECKBOXES) {
 		box.addEventListener('change', function () {
+			let task, currentTasksPriority;
+			const currentTaskName = box.nextElementSibling.textContent;
+			if (box.parentElement.parentElement.className === 'todo__priority--high') {
+				currentTasksPriority = 'high';
+			} else {
+				currentTasksPriority = 'low';
+			}
 			if (box.checked === true) {
-				box.parentElement.classList.add('done')
+				box.parentElement.classList.add('done');
+				task = new Task(currentTaskName, currentTasksPriority);
+				task.status = DONE;
+				console.log(task);
+				localStorage.setItem(currentTaskName, JSON.stringify(task));
 			} else {
 				box.parentElement.classList.remove('done')
+				task = new Task(currentTaskName, currentTasksPriority);
+				task.status = IN_PROGRESS;
+				localStorage.setItem(currentTaskName, JSON.stringify(task));
 			}
+			console.log(taskList);
 		});
 	}
 	return CHECKBOXES, BUTTONS_DEL;
 }
 
+function Task(name, priority) {
+	this.name = name;
+	this.priority = priority;
+	this.status = IN_PROGRESS;
+}
 
-function allTasksAdd() {
+function tasksAdd() {
 	for (let form of TODO_UI.FORMS_ADD) {
 		form.addEventListener('submit', function (event) {
 			event.preventDefault();
-			if (form.firstElementChild.value === "") {
+			const formValue = form.firstElementChild.value;
+			if (formValue === "") {
 				alert('Add task please');
-			} else if (form.className === 'high') {
-				taskAdd('high');
-			} else taskAdd('low');
+			} else if (localStorage.getItem(formValue) !== null) {
+				alert('This task is already in the list');
+			} else {
+				let name = form.firstElementChild.value;
+				form.firstElementChild.value = "";
+				let priority = form.className;
+				let newTask = new Task(name, priority);
+				localStorage.setItem(name, JSON.stringify(newTask));
+				let div = createTaskNode(newTask.name);
+				if (form.className === 'high') {
+					TODO_UI.HIGH.append(div);
+				} else {
+					TODO_UI.LOW.append(div);
+				}
+				CHECKBOXES = document.querySelectorAll('.box');
+				BUTTONS_DEL = document.querySelectorAll('.todo__btn--del');
+				checkedStyle();
+				taskDelete();
+				return CHECKBOXES, BUTTONS_DEL;
+			}
 		});
 	}
-}
-
-function taskAdd(prior) {
-	let selector = `.${prior}`;
-	const targetForm = document.querySelector(selector);
-	let newTask = {};
-	newTask.name = targetForm.firstElementChild.value;
-	targetForm.firstElementChild.value = "";
-	newTask.priority = prior;
-	taskList.push(newTask);
-	let div = createTaskNode(newTask.name);
-	if (prior === 'high') {
-		TODO_UI.HIGH.append(div);
-	} else {
-		TODO_UI.LOW.append(div);
-	}
-	CHECKBOXES = document.querySelectorAll('.box');
-	BUTTONS_DEL = document.querySelectorAll('.todo__btn--del');
-	checkedStyle();
-	taskDelete();
-	return taskList, CHECKBOXES, BUTTONS_DEL;
 }
--- js/view.js
+++ js/view.js
@@ -5,10 +5,11 @@ export const TODO_UI = {
 	FORMS_ADD: document.querySelectorAll('form'),
 }
 
-export function createTaskNode(name) {
+export function createTaskNode(name, isChecked) {
 	let div = document.createElement('div');
 	div.classList.add("task");
 	let input = document.createElement('input');
+	input.checked = isChecked;
 	input.classList.add('box');
 	input.setAttribute('type', 'checkbox');
 	let p = document.createElement('p');
@@ -16,6 +17,9 @@ export function createTaskNode(name) {
 	let button = document.createElement('button');
 	button.classList.add('todo__btn--del');
 	div.append(input);
+	if (input.checked === true) {
+		input.parentElement.classList.add('done');
+	}
 	div.append(p);
 	div.append(button);
 	return div;
